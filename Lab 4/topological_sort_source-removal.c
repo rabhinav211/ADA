2e0dd692642086085a289b9009e8a6bb539e7495
rabhinav211 <153578832+rabhinav211@users.noreply.github.com>
2024-07-13 21:57:17 +0530

    Update topological_sort_source-removal.c

--- Lab 4/topological_sort_source-removal.c
+++ Lab 4/topological_sort_source-removal.c
@@ -1,80 +1,85 @@
 #include <stdio.h>
 #define v 100
-int top=-1;
-void indegree(int a_matrix[v][v],int n,int in[v])
+int top = -1;
+
+void indegree(int a_matrix[v][v], int n, int in[v])
 {
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0;j<n;j++)
+        for (int j = 0; j < n; j++)
         {
-            if(a_matrix[i][j])
+            if (a_matrix[i][j])
             {
                 in[j]++;
             }
         }
     }
 }
-void toposort(int a_matrix[v][v],int n)
+
+void toposort(int a_matrix[v][v], int n)
 {
-    int in[v]={0};
+    int in[v] = {0};
     int topo[v];
-    int k=0;
-
-    int s[v]={0};
-    indegree(a_matrix,n,in);
-    for(int i=0;i<n;i++)
+    int k = 0;
+    int s[v] = {0};
+    
+    indegree(a_matrix, n, in);
+    
+    for (int i = 0; i < n; i++)
     {
-        if(in[i]==0)
+        if (in[i] == 0)
         {
-           top++;
-           s[top]=i;
-
+            top++;
+            s[top] = i;
         }
     }
-    while(top!=-1)
+    
+    while (top != -1)
     {
-        int vertex=s[top];
+        int vertex = s[top];
         top--;
-        topo[k++]=vertex;
-        for(int i=0;i<n;i++)
+        topo[k++] = vertex;
+        
+        for (int i = 0; i < n; i++)
         {
-            if(a_matrix[vertex][i])
+            if (a_matrix[vertex][i])
             {
                 in[i]--;
-                if(in[i]==0)
+                if (in[i] == 0)
                 {
                     top++;
-                    s[top]=i;
+                    s[top] = i;
                 }
             }
         }
     }
-    if(k!=n)
+    
+    if (k != n)
     {
         printf("cycle exists");
     }
-    else{
-    printf("the topological sort:");
-    for(int i=0;i<n;i++)
+    else
     {
-        printf("%d ",topo[i]+1);
-    }
-    }
-}
-int main()
-{
-    int a_matrix[v][v];
-    int n;
-    printf("enter the no of vertices:");
-    scanf("%d",&n);
-    printf("enter the adjaceny matrix:\n");
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n;j++)
+        printf("the topological sort: ");
+        for (int i = 0; i < n; i++)
         {
-            scanf("%d",&a_matrix[i][j]);
+            printf("%d ", topo[i] + 1);
         }
     }
-    toposort(a_matrix,n);
+}
+
+int main()
+{
+    int a_matrix[v][v] = {
+        {0, 1, 0, 0, 0, 1},
+        {0, 0, 1, 1, 0, 0},
+        {0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0}
+    };
+    int n = 6; 
+
+    toposort(a_matrix, n);
     return 0;
 }
